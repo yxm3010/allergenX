5c1ff746827c0ad615c63c61558cd46776558ef4
Yuta Morioka <yutamorioka@Yutas-MBP.home>
2023-09-24 18:24:51 -0700

    Adding DB edit feature

--- static/html2flask.js
+++ static/html2flask.js
@@ -36,10 +36,6 @@ function addrow() {
         success: location.reload()
       });
     
-    // add row to table
-
-    
-    
     // clear form
     document.getElementById("itemname").value = "";
     document.getElementById("eggCheck").checked = false;
@@ -60,7 +56,7 @@ function removerow(){
         i += 1;
     });
     
-    if(confirm('Are you sure to delete the following from the database?\n ${items}') == true){
+    if(confirm('Are you sure to delete the items from the database?') == true){
 
         var serverData = [
             {"customerID": 0},
@@ -78,13 +74,137 @@ function removerow(){
     }
 }
 
+function editrow(ctl){
+    // https://www.codemag.com/article/1511031/CRUD-in-HTML-JavaScript-and-jQuery
+
+    // Set the following variables as global variables so that they can be accessible by modal 
+    var currentRow = $(ctl).parents("tr");
+    var cols = currentRow.children("td");
+    var edititemname = $(cols[2]).text();
+    var editEggScore = $(cols[3]).text();
+    var editNutsScore = $(cols[4]).text();
+    var editMilkScore = $(cols[5]).text();
+    var editWheatScore = $(cols[6]).text();
+    var editFishScore = $(cols[7]).text();
+    var editShellfishScore = $(cols[8]).text();
+    var editSesamiScore = $(cols[9]).text();
+    var editPeanutsScore = $(cols[10]).text();
+    var editSoyScore = $(cols[11]).text();
+
+    $('#editModal').modal('show');
+   
+    $('#editModal').on('shown.bs.modal', function (event) {
+        $('#itemname').val(edititemname);
+
+        if(editEggScore=='X'){
+            $("#eggCheck").prop('checked',true);
+            $('#eggOptions').val('No');
+        }else if(editEggScore=='*'){
+            $("#eggCheck").prop('checked',true);
+            $('#eggOptions').val('Yes');
+        }else{
+            $("#eggCheck").prop('checked',false);
+            $('#eggOptions').val('No');
+        }
+
+        if(editNutsScore=='X'){
+            $("#nutsCheck").prop('checked',true);
+            $('#nutsOptions').val('No');
+        }else if(editNutsScore=='*'){
+            $("#nutsCheck").prop('checked',true);
+            $('#nutsOptions').val('Yes');
+        }else{
+            $("#nutsCheck").prop('checked',false);
+            $('#nutsOptions').val('No');
+        }
+
+        if(editMilkScore=='X'){
+            $("#milkCheck").prop('checked',true);
+            $('#milkOptions').val('No');
+        }else if(editMilkScore=='*'){
+            $("#milkCheck").prop('checked',true);
+            $('#milkOptions').val('Yes');
+        }else{
+            $("#milkCheck").prop('checked',false);
+            $('#milkOptions').val('No');
+        }
+
+        if(editWheatScore=='X'){
+            $("#wheatCheck").prop('checked',true);
+            $('#wheatOptions').val('No');
+        }else if(editWheatScore=='*'){
+            $("#wheatCheck").prop('checked',true);
+            $('#wheatOptions').val('Yes');
+        }else{
+            $("#wheatCheck").prop('checked',false);
+            $('#wheatOptions').val('No');
+        }
+
+        if(editFishScore=='X'){
+            $("#fishCheck").prop('checked',true);
+            $('#fishOptions').val('No');
+        }else if(editFishScore=='*'){
+            $("#fishCheck").prop('checked',true);
+            $('#fishOptions').val('Yes');
+        }else{
+            $("#fishCheck").prop('checked',false);
+            $('#fishOptions').val('No');
+        }
+
+        if(editShellfishScore=='X'){
+            $("#shellfishCheck").prop('checked',true);
+            $('#shellfishOptions').val('No');
+        }else if(editShellfishScore=='*'){
+            $("#shellfishCheck").prop('checked',true);
+            $('#shellfishOptions').val('Yes');
+        }else{
+            $("#shellfishCheck").prop('checked',false);
+            $('#shellfishOptions').val('No');
+        }
+
+        if(editSesamiScore=='X'){
+            $("#sesamiCheck").prop('checked',true);
+            $('#sesamiOptions').val('No');
+        }else if(editSesamiScore=='*'){
+            $("#sesamiCheck").prop('checked',true);
+            $('#sesamiOptions').val('Yes');
+        }else{
+            $("#sesamiCheck").prop('checked',false);
+            $('#sesamiOptions').val('No');
+        }
+
+        if(editPeanutsScore=='X'){
+            $("#peanutsCheck").prop('checked',true);
+            $('#peanutsOptions').val('No');
+        }else if(editPeanutsScore=='*'){
+            $("#peanutsCheck").prop('checked',true);
+            $('#peanutsOptions').val('Yes');
+        }else{
+            $("#peanutsCheck").prop('checked',false);
+            $('#peanutsOptions').val('No');
+        }
+
+        if(editSoyScore=='X'){
+            $("#soyCheck").prop('checked',true);
+            $('#soyOptions').val('No');
+        }else if(editSoyScore=='*'){
+            $("#soyCheck").prop('checked',true);
+            $('#soyOptions').val('Yes');
+        }else{
+            $("#soyCheck").prop('checked',false);
+            $('#soyOptions').val('No');
+        }
+
+    })
+}
+
 function score2symbol(score){
     if(score==100){
         return ""
     }else if(score==50){
-        return "<i style='color:orange' class='bi bi-x-octagon'></i>"
+        return "*"
     }else{
-        return "<i style='color:red' class='bi bi-x-octagon-fill'></i>";
+        return "X";
     }
 }
 
