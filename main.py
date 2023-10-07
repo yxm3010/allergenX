7f603522dc774ba864d851f6bee4dc021ab79651
Yuta Morioka <yutamorioka@Yutas-MBP.home>
2023-10-07 07:59:56 -0700

    setting up venv

--- main.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-def make_qrcode(cust, url):
-    img = qrcode.make(url)
-    fname = cust + '.png'
-    img.save(fname)
-
-make_qrcode('mcdonalds','https://corporate.mcdonalds.com/corpmcd/home.html')
-
